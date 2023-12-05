5304a789657c6a9835f8920163d6f425c33b8a8f
Kian Bonakdar <bonakdar575@gmail.com>
2023-12-05 15:22:40 -0800

    Added test for temperature change

---
+++ src/test/java/TestingTemperatureButtonsTest.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestingTemperatureButtonsTest {
+
+    @Test
+    public void testTemperatureChanges() {
+        // Test the increase temperature functionality
+        assertEquals(25, increaseTemperature(20));
+
+        // Test the decrease temperature functionality
+        assertEquals(15, decreaseTemperature(20));
+    }
+
+    @Test
+    public void testDecreaseTemperature() {
+        // Test the decrease temperature functionality
+        assertEquals(18, decreaseTemperature(20));
+        assertEquals(5, decreaseTemperature(10));
+    }
+
+    @Test
+    public void testIncreaseTemperature() {
+        // Test the increase temperature functionality
+        assertEquals(22, increaseTemperature(20));
+        assertEquals(30, increaseTemperature(25));
+    }
+
+    // Utility method to simulate the decrease temperature functionality
+    private int decreaseTemperature(int currentTemperature) {
+        // Decrease temperature functionality
+        // For simplicity, let's assume a constant decrement of 2 degrees
+        int decreasedTemperature = currentTemperature - 2;
+        // Assert that the expected value matches the current temperature
+        assertEquals(decreasedTemperature, currentTemperature - 2);
+        return decreasedTemperature;
+    }
+
+    // Utility method to simulate the increase temperature functionality
+    private int increaseTemperature(int currentTemperature) {
+        // Increase temperature functionality
+        // For simplicity, let's assume a constant increment of 2 degrees
+        int increasedTemperature = currentTemperature + 2;
+        // Assert that the expected value matches the current temperature
+        assertEquals(increasedTemperature, currentTemperature + 2);
+        return increasedTemperature;
+    }
+}
