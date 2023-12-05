e5700ea343d74c64378dad1d9bd1e1b5ff8295af
Andres <dev+git@andrescruz.me>
2023-12-04 18:40:13 -0800

    Simple first test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static org.junit.Assert.*;
+
+public class SampleTest {
+    @Test
+    public void testSquareRoot() {
+        assertTrue(Math.pow(2, 4) == 16);
+    }
+}
